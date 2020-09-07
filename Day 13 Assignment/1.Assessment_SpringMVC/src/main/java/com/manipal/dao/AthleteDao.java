cf82d16f30a2e498ef65e188c7afc5fda7294e90
gaurav94c <70712582+gaurav94c@users.noreply.github.com>
2020-09-07 23:55:37 +0530

    Add files via upload

---
+++ Day 13 Assignment/1.Assessment_SpringMVC/src/main/java/com/manipal/dao/AthleteDao.java
@@ -0,0 +1,51 @@
+package com.manipal.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.manipal.model.Athlete;
+
+public class AthleteDao {
+	
+	JdbcTemplate jdbctemplate;
+
+	public void setTemplate(JdbcTemplate jdbctemplate) {
+		this.jdbctemplate = jdbctemplate;
+	}
+
+	public int save(Athlete p) {
+		
+		String sql = "insert into athlete(name,gender,email,mobile) values('" + p.getName() + "','" + p.getGender() + "','"
+				+ p.getEmail() +"','"+p.getMobile()+"')";
+		return jdbctemplate.update(sql);
+		
+	}
+
+	public int update(Athlete p) {
+
+	String sql = "update athlete set name='" + p.getName() + "', gender=" + p.getGender() + ",email='"
+				+ p.getEmail()+ ","+ ",mobile='"+p.getMobile();
+		return jdbctemplate.update(sql);
+	}
+
+	
+	public List<Athlete> getAthletes() {
+		return jdbctemplate.query("select * from athlete", new RowMapper<Athlete>() {
+			public Athlete mapRow(ResultSet rs, int row) throws SQLException {
+				Athlete e = new Athlete();
+				Random rand=new Random();
+				e.setId((int) rand.nextInt(800)+1000);
+				e.setName(rs.getString(2));
+				e.setGender(rs.getString(3));
+				e.setEmail(rs.getString(4));
+				e.setMobile(rs.getString(5));
+				return e;
+			}
+		});
+	}
+}
---
+++ Day 13 Assignment/1.Assessment_SpringMVC/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Spring MVC</title>
+</head>
+<body>
+	<a href="athleteform?jsp" style="font-size: 30px;">Add an Athlete</a><br>
+	<a href="viewathlete" style="font-size: 30px; ">Show all Athlete Details</a>
+</body>
+</html>
