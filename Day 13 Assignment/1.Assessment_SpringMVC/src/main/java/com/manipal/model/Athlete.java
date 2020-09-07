76fa047c8076844ffe2f9ab3b388e1b2c0a11f81
gaurav94c <70712582+gaurav94c@users.noreply.github.com>
2020-09-07 23:28:22 +0530

    Add files via upload

---
+++ Day 13 Assignment/1.Assessment_SpringMVC/src/main/java/com/manipal/model/Athlete.java
@@ -0,0 +1,51 @@
+package com.manipal.model;
+
+public class Athlete {
+
+	private int id;
+	private String name;
+	private String gender;
+	private String email;
+	private String mobile;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+}
---
+++ Day 13 Assignment/1.Assessment_SpringMVC/src/main/webapp/WEB-INF/jsps/loginform.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<!-- The purpose of the HTML action attribute is to specify the URL of a form  -->
+
+<form action="loginform_url">
+	Name : <input type="text" name="studentName"/> <br/>
+	Password : <input type="text" name="studentPassword"/> <br/>
+	<input type="submit" value="Submit" />
+</form>
+
+</body>
+</html>
